d45affe4f9b2676e851c94735fd5f862d6245821
Josue Barrios Rodriguez <josue.barrios@gmail.com>
2017-12-18 23:03:22 -0500

    commit inicial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { MaterializeModule } from 'angular2-materialize';
 
 import { AppComponent } from './app.component';
-
+import { PreguntaComponent } from './componentes/pregunta';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PreguntaComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MaterializeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
